4adb5cc61a281a1f4a9f6596062eeaecde1bc639
Raj-Mayank2 <vmayank0123@gmail.com>
2024-08-14 12:18:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [color,setColor]=useState('olive');
+  function changeColor(color){
+    setColor(color)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+          onClick={()=>changeColor('red')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black' style={{backgroundColor:'red'}}
+          >Red</button>
+          <button
+          onClick={()=>changeColor('blue')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor:'blue'}}
+          >Blue</button>
+          <button
+          onClick={()=>changeColor('yellow')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor:'yellow'}}
+          >yellow</button>
+          <button
+          onClick={()=>changeColor('grey')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor:'grey'}}
+          >grey</button>
+          <button
+          onClick={()=>changeColor('pink')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor:'pink'}}
+          >pink</button>
+
+        </div>
+      </div>
     </div>
   );
 }
